e129b7ff3b1d3ec4b043820356dfadc645a8a9d2
duduialaAlexandru <alexandru24@live.com>
2018-12-22 16:41:02 +0200

    Added route to update todo

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const {ObjectID} = require('mongodb')
+const _ = require('lodash')
+
 const {mongoose} = require('./db/mongoose')
 const {Todo} = require('./models/todo')
 const {UserEmail} = require('./models/user')
@@ -8,7 +10,10 @@ const {UserEmail} = require('./models/user')
 const app = express()
 const port = process.env.PORT || 3000
 
+// use midleware bodyparser to parse objects to json
 app.use(bodyParser.json())
+
+// route to create and save a todo to the database
 app.post('/todos', (req, res)=>{
     const newTodo = new Todo({
         text: req.body.text
@@ -20,6 +25,7 @@ app.post('/todos', (req, res)=>{
     })
 })
 
+// route to get all todos from database
 app.get('/todos', (req, res)=>{
     Todo.find().then((todos)=>{
         res.send({todos})
@@ -28,6 +34,8 @@ app.get('/todos', (req, res)=>{
     res.status(400).send(e)
 })
 
+
+// route to get todo by id from database
 app.get('/todos/:id', (req, res)=>{
     const id = req.params.id
     if(!ObjectID.isValid(id)){
@@ -43,6 +51,7 @@ app.get('/todos/:id', (req, res)=>{
     })
 })
 
+// route to find todo by id and delete it and returning the deleted todo
 app.delete('/delete/:id', (req, res)=>{
     const id = req.params.id
     if(!ObjectID.isValid(id)){
@@ -56,6 +65,33 @@ app.delete('/delete/:id', (req, res)=>{
     }).catch((e)=>{
         res.status(400).send()
     })
+})
+
+// route to find a todo by id and update it
+app.patch('/todos/:id', (req, res)=>{
+    const id = req.params.id
+    const body = _.pick(req.body, ['text', 'completed'])
+
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send()
+    }
+
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime()
+    } else {
+        body.completed = false
+        body.completedAt = null
+    }
+
+    Todo.findOneAndUpdate(id, {$set: body}, {new: true}).then((todo)=>{
+        if(!todo){
+            return res.status(404).send()
+        }
+        res.status(200).send({todo})
+    }).catch((e)=>{
+        res.status(400).send()
+    })
+
 })
 
 app.listen(port, ()=>{
